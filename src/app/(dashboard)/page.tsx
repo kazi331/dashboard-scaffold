127c8142908a4813d72574654572edf790b116e8
kazi331 <kazisharifulislam52@gmail.com>
2024-12-19 15:12:48 +0600

    layout  added

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,102 @@
+export default function Page() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 pt-0">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted" />
+      </div>
+      <div className="">
+        Lorem, ipsum dolor. Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ullam debitis consectetur praesentium fuga repellat hic architecto
+        alias autem exercitationem ad, aspernatur necessitatibus, natus iste cum
+        expedita placeat laboriosam magni aut adipisci eaque corrupti voluptate,
+        eius nulla. Deleniti eos aspernatur harum, quo a amet aliquam velit
+        totam repudiandae ratione fuga veritatis perspiciatis error commodi ipsa
+        cumque nemo possimus ad. Aliquam sed quas omnis unde magni repellat
+        incidunt quam nam quo? At perferendis esse quaerat voluptate unde
+        suscipit dignissimos amet nemo, iure, sed provident aspernatur sapiente
+        error. Quo similique non id officia eaque, ut, architecto, sapiente enim
+        animi placeat saepe voluptates veniam facere amet ratione voluptatibus
+        odio alias velit blanditiis dicta excepturi suscipit! Quo perspiciatis
+        ad placeat, aspernatur similique recusandae corporis optio nam tenetur,
+        non error est eligendi! Laudantium soluta necessitatibus quam veritatis
+        pariatur exercitationem reprehenderit architecto molestiae beatae
+        voluptatem. Voluptatibus dolorem dolores illum deleniti nihil cumque
+        debitis laudantium numquam, quis facere error maxime ex assumenda
+        suscipit possimus, labore sapiente adipisci, eveniet expedita laboriosam
+        magni! Blanditiis eos quia dicta dignissimos a voluptate, at
+        consequuntur nostrum cumque minus qui velit accusamus amet repudiandae.
+        Atque quidem ipsam vero debitis tenetur iusto provident nobis magnam.
+        Illum quia veniam blanditiis fugiat mollitia laborum corporis
+        consectetur commodi praesentium! Totam eum suscipit, molestias laborum
+        cumque nemo possimus ad. Aliquam sed quas omnis unde magni repellat
+        incidunt quam nam quo? At perferendis esse quaerat voluptate unde
+        suscipit dignissimos amet nemo, iure, sed provident aspernatur sapiente
+        error. Quo similique non id officia eaque, ut, architecto, sapiente enim
+        animi placeat saepe voluptates veniam facere amet ratione voluptatibus
+        odio alias velit blanditiis dicta excepturi suscipit! Quo perspiciatis
+        ad placeat, aspernatur similique recusandae corporis optio nam tenetur,
+        non error est eligendi! Laudantium soluta necessitatibus quam veritatis
+        pariatur exercitationem reprehenderit architecto molestiae beatae
+        voluptatem. Voluptatibus dolorem dolores illum deleniti nihil cumque
+        debitis laudantium numquam, quis facere error maxime ex assumenda
+        suscipit possimus, labore sapiente adipisci, eveniet expedita laboriosam
+        magni! Blanditiis eos quia dicta dignissimos a voluptate, at
+        consequuntur nostrum cumque minus qui velit accusamus amet repudiandae.
+        Atque quidem ipsam vero debitis tenetur iusto provident nobis magnam.
+        Illum quia veniam blanditiis fugiat mollitia laborum corporis
+        consectetur commodi praesentium! Totam eum suscipit, molestias laborum
+        cumque nemo possimus ad. Aliquam sed quas omnis unde magni repellat
+        incidunt quam nam quo? At perferendis esse quaerat voluptate unde
+        suscipit dignissimos amet nemo, iure, sed provident aspernatur sapiente
+        error. Quo similique non id officia eaque, ut, architecto, sapiente enim
+        animi placeat saepe voluptates veniam facere amet ratione voluptatibus
+        odio alias velit blanditiis dicta excepturi suscipit! Quo perspiciatis
+        ad placeat, aspernatur similique recusandae corporis optio nam tenetur,
+        non error est eligendi! Laudantium soluta necessitatibus quam veritatis
+        pariatur exercitationem reprehenderit architecto molestiae beatae
+        voluptatem. Voluptatibus dolorem dolores illum deleniti nihil cumque
+        debitis laudantium numquam, quis facere error maxime ex assumenda
+        suscipit possimus, labore sapiente adipisci, eveniet expedita laboriosam
+        magni! Blanditiis eos quia dicta dignissimos a voluptate, at
+        consequuntur nostrum cumque minus qui velit accusamus amet repudiandae.
+        Atque quidem ipsam vero debitis tenetur iusto provident nobis magnam.
+        Illum quia veniam blanditiis fugiat mollitia laborum corporis
+        consectetur commodi praesentium! Totam eum suscipit, molestias laborum
+        cumque nemo possimus ad. Aliquam sed quas omnis unde magni repellat
+        incidunt quam nam quo? At perferendis esse quaerat voluptate unde
+        suscipit dignissimos amet nemo, iure, sed provident aspernatur sapiente
+        error. Quo similique non id officia eaque, ut, architecto, sapiente enim
+        animi placeat saepe voluptates veniam facere amet ratione voluptatibus
+        odio alias velit blanditiis dicta excepturi suscipit! Quo perspiciatis
+        ad placeat, aspernatur similique recusandae corporis optio nam tenetur,
+        non error est eligendi! Laudantium soluta necessitatibus quam veritatis
+        pariatur exercitationem reprehenderit architecto molestiae beatae
+        voluptatem. Voluptatibus dolorem dolores illum deleniti nihil cumque
+        debitis laudantium numquam, quis facere error maxime ex assumenda
+        suscipit possimus, labore sapiente adipisci, eveniet expedita laboriosam
+        magni! Blanditiis eos quia dicta dignissimos a voluptate, at
+        consequuntur nostrum cumque minus qui velit accusamus amet repudiandae.
+        Atque quidem ipsam vero debitis tenetur iusto provident nobis magnam.
+        Illum quia veniam blanditiis fugiat mollitia laborum corporis
+        consectetur commodi praesentium! Totam eum suscipit, molestias laborum
+        ducimus dolor nostrum vitae praesentium dicta at quod quae similique
+        dolores repellendus numquam ipsa iure labore ullam pariatur possimus
+        explicabo harurovident molestias, alias, maiores, tempore veniam
+        architecto culpa doloribus vero voluptate maxime aliquid fugit voluptas
+        velit laudantium totam iste similique deleniti incidunt excepturi
+        corrupti nostrum accusantium a eligendi ratione. Quibusdam dolores, ut
+        repellat cum dignissimos voluptatem facere enim? Perferendis voluptate
+        perspiciatis ipsam iusto amet me labore ullam pariatur possimus
+        explicabo harurovident molestias, alias, maiores, tempore veniam
+        architecto culpa doloribus vero voluptate maxime aliquid fugit voluptas
+        velit laudantium totam iste similique deleniti incidunt excepturi
+        corrupti nostrum accusantium a eligendi ratione. Quibusdam dolores, ut
+        repellat cum dignissimos voluptatem facere enim? Perferendis voluptate
+        perspiciatis ipsam iusto amet modi minima laboriosam ea vitae quia
+        adipisci animi optio incidunt, inventore doloremque!
+      </div>
+    </div>
+  );
+}
