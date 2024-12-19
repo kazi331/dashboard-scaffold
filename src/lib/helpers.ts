82462ec3afebad204c0e08b13809aefc587b40df
kazi331 <kazisharifulislam52@gmail.com>
2024-12-19 21:18:02 +0600

    pushing buggy code

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const nameInitials = (name: string) => {
   return name
     .split(" ")
@@ -11,3 +13,6 @@ export const placeholderAvatar = (size: number = 300) => {
 export const placeholderImage = (height: number = 500, width: number = 500) => {
   return `https://picsum.photos/seed/picsum/${height}/${width}`;
 };
+
+// axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+// axios.defaults.withCredentials = true;
